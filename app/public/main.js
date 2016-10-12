233db91d853a7dfe79b90aa1c0258922e6e95a6e
Xavier Ho <contact@xavierho.com>
2016-10-12 15:14:08 +1100

    Adding interest questions on page

--- app/public/main.js
+++ app/public/main.js
@@ -58,3 +58,41 @@ our_picks_list.forEach( function (item) {
 
   our_picks.appendChild(container);
 });
+
+var interest_list = [
+  "Development",
+  "Cycling",
+  "Community gardens",
+  "Walking my dog",
+];
+
+var create_interest_card = function (name) {
+
+  // Creating interest cards
+  var card = document.createElement('div');
+  card.className = 'interest';
+  var flexbox = document.createElement('div');
+  flexbox.className = 'flex-center';
+  var text = document.createElement('span');
+  text.textContent = name;
+  flexbox.appendChild(text);
+  card.appendChild(flexbox);
+
+  // Selecting interests
+  card.addEventListener('click', function (event) {
+    card.classList.toggle('selected');
+  });
+
+  return card;
+};
+
+// entry point - onboarding
+var onboard_interest = document.getElementById('onboard_interest_cards');
+interest_list.forEach( function (interest) {
+  var interest_card = create_interest_card(interest);
+  onboard_interest.appendChild(interest_card);
+});
+
+var submit_onboard = function () {
+  window.location = window.location.origin + '/index.html';
+};
